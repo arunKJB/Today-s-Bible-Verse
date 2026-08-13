17d9ed443f8ec9346f3b25bf059ea6861898daa4
arunKJB <arunantony.kjb@gmail.com>
2026-08-13 23:05:08 +0530

    Add new verses for days 14 to 31 and September 1 to 6
    Added new verses for days 14 to 31 in verses.js.

--- verses.js
+++ verses.js
@@ -29,6 +29,52 @@ const verses = [
         englishVerse:
             "The teaching of the wise is a fountain of life, turning men away from the nets of death."
     },
+    
+    {
+        day: 14,
+
+        reference: "நீதிமொழிகள் 14:26",
+
+        verse:
+            "ஆண்டவரிடம் கொள்ளும் அச்சம் ஒருவருக்குத் திடநம்பிக்கை அளிக்கும்; அவர் தம்முடைய பிள்ளைகளுக்கு அடைக்கலமா யிருப்பார்.",
+
+        englishReference:
+            "Proverbs 14:26",
+
+        englishVerse:
+            "For him in whose heart is the fear of the Lord there is strong hope: and his children will have a safe place."
+    },
+    
+    {
+        day: 15,
+
+        reference: "நீதிமொழிகள் 15:1",
+
+        verse:
+            "கனிவான மறுமொழி கடுஞ்சினத்தையும் ஆற்றிவிடும்; கடுஞ்சொல்லோ சினத்தை எழுப்பும்.",
+
+        englishReference:
+            "Proverbs 15:1",
+
+        englishVerse:
+            "By a soft answer wrath is turned away, but a bitter word is a cause of angry feelings."
+    },
+
+
+    {
+        day: 16,
+
+        reference: "நீதிமொழிகள் 16:3",
+
+        verse:
+            "உன் செயல்களை ஆண்டவரிடம் ஒப்படை; அவற்றை வெற்றியுடன் நிறைவேற்றுவாய்.",
+
+        englishReference:
+            "Proverbs 16:3",
+
+        englishVerse:
+            "Put your works into the hands of the Lord, and your purposes will be made certain."
+    },
 
     {
         day: 17,
@@ -43,6 +89,96 @@ const verses = [
 
         englishVerse:
             "Be strong, then ,and be valiant.Do not tremble or be afraid, because the Lord, your God, is with you wherever you go."
+    },
+
+    {
+        day: 18,
+
+        reference: "நீதிமொழிகள் 18:10",
+
+        verse:
+            "ஆண்டவரது திருப்பெயர் உறுதியான கோட்டை; அவருக்கு அஞ்சி நடப்பவர் அதனுட் சென்று அடைக்கலம் பெறுவார்.",
+
+        englishReference:
+            "Proverbs 18:10",
+
+        englishVerse:
+            "The name of the Lord is a strong tower: the upright man running into it is safe."
+    },
+
+    {
+        day: 19,
+
+        reference: "நீதிமொழிகள் 19:21",
+
+        verse:
+            "மனிதர் மனத்தில் எழும் எண்ணங்கள் ஏராளம்; ஆனால் ஆண்டவரது திட்டமே நிலைத்துநிற்கும்.",
+
+        englishReference:
+            "Proverbs 19:21",
+
+        englishVerse:
+            "A man’s heart may be full of designs, but the purpose of the Lord is unchanging."
+    },
+
+    {
+        day: 20,
+
+        reference: "நீதிமொழிகள் 20:24",
+
+        verse:
+            "மனிதனுடைய வாழ்க்கைப் பாதையை ஆண்டவர் அமைக்கின்றார்; அப்படியிருக்க, தன் வழியை மனிதனால் எப்படி அறிய இயலும்?",
+
+        englishReference:
+            "Proverbs 20:24",
+
+        englishVerse:
+            "A man’s steps are of the Lord; how then may a man have knowledge of his way?"
+    },
+
+     {
+        day: 21,
+
+        reference: "நீதிமொழிகள் 21:21",
+
+        verse:
+            "நேர்மையையும் இரக்கத்தையும் கடைப் பிடித்து நடப்பவர், நீடித்து வாழ்வார், மேன்மையும் அடைவார்.",
+
+        englishReference:
+            "Proverbs 21:21",
+
+        englishVerse:
+            "He who goes after righteousness and mercy will get life, righteousness, and honour."
+    },
+
+    {
+        day: 22,
+
+        reference: "நீதிமொழிகள் 22:4",
+
+        verse:
+            "தாழ்மையுள்ளவர்களுக்கும் ஆண்டவரிடம் அச்சம் உடையவர்களுக்கும் கிடைக்கும் பயன் செல்வமும் மேன்மையும் நீடித்த ஆயுளுமாகும்.",
+
+        englishReference:
+            "Proverbs 22:4",
+
+        englishVerse:
+            "The reward of a gentle spirit and the fear of the Lord is wealth and honour and life."
+    },
+
+    {
+        day: 23,
+
+        reference: "நீதிமொழிகள் 23:26",
+
+        verse:
+            "மகனே, நான் சொல்வதைக் கவனி; என் வழிகளில் உன் கவனத்தைச் செலுத்து.",
+
+        englishReference:
+            "Proverbs 23:26",
+
+        englishVerse:
+            "My son, give me your heart, and let your eyes take delight in my ways."
     },
 
      {
@@ -61,6 +197,96 @@ const verses = [
     },
 
     {
+        day: 25,
+
+        reference: "நீதிமொழிகள் 25:11",
+
+        verse:
+            "தக்க வேளையில் சொன்ன சொல் வெள்ளித் தட்டில் வைத்த பொற்கனிக்குச் சமம்.",
+
+        englishReference:
+            "Proverbs 25:11",
+
+        englishVerse:
+            "A word at the right time is like apples of gold in a network of silver."
+    },
+
+    {
+        day: 26,
+
+        reference: "நீதிமொழிகள் 26:5",
+
+        verse:
+            "மடையரின் கேள்விக்கு அவரது மடமையை உணர்த்தும் வகையில் பதிலுரை; இல்லாவிடில், அவர் தம்மை ஞானி என்று எண்ணிக் கொள்வார்;",
+
+        englishReference:
+            "Proverbs 26:5",
+
+        englishVerse:
+            "Give a foolish man a foolish answer, or he will seem wise to himself."
+    },
+
+    {
+        day: 27,
+
+        reference: "நீதிமொழிகள் 27:17",
+
+        verse:
+            "இரும்பை இரும்பு கூர்மையாக்குவது போல, ஒருவர்தம் அறிவால் மற்றவரைக் கூர் மதியாளராக்கலாம்.",
+
+        englishReference:
+            "Proverbs 27:17",
+
+        englishVerse:
+            "Iron makes iron sharp; so a man makes sharp his friend."
+    },
+
+    {
+        day: 28,
+
+        reference: "நீதிமொழிகள் 28:14",
+
+        verse:
+            "எப்போதும் கடவுளுக்கு அஞ்சி வாழ்பவர் நற்பேறுபெறுவார்; பிடிவாதமுள்ளவரோ தீங்கிற்கு உள்ளாவார்.",
+
+        englishReference:
+            "Proverbs 28:14",
+
+        englishVerse:
+            "Happy is the man in whom is the fear of the Lord at all times; but he whose heart is hard will come into trouble."
+    },
+
+    {
+        day: 29,
+
+        reference: "நீதிமொழிகள் 29:25",
+
+        verse:
+            "பிறர் என்ன சொல்வார்களோ என்று அஞ்சிக்கொண்டு நடப்பவர் கண்ணியில் சிக்கிக்கொள்வார்; ஆண்டவரை நம்புகிற வருக்கோ அடைக்கலம் கிடைக்கும்.",
+
+        englishReference:
+            "Proverbs 29:25",
+
+        englishVerse:
+            "The fear of man is a cause of danger: but whoever puts his faith in the Lord will have a safe place on high."
+    },
+
+    {
+        day: 30,
+
+        reference: "நீதிமொழிகள் 30:5",
+
+        verse:
+            "கடவுளின் ஒவ்வொரு வாக்கும் பரிசோதிக் கப்பட்டு நம்பத்தக்கதாய் விளங்குகிறது; தம்மை அடைக்கலமாகக் கொண்டவர்களுக்கு அவர் கேடயமாயிருக்கிறார்.",
+
+        englishReference:
+            "Proverbs 30:5",
+
+        englishVerse:
+            "Every word of God is tested: he is a breastplate to those who put their faith in him."
+    },
+
+    {
         day: 31,
 
         reference: "யோவேல் 2:26",
@@ -73,6 +299,96 @@ const verses = [
 
         englishVerse:
             "You shall eat in plenty and be satisfied, and praise the name of the LORD your God, who has dealt wondrously with you. And my people shall never again be put to shame."
+    },
+
+    {
+        day: 1,
+
+        reference: "நீதிமொழிகள் 1:7",
+
+        verse:
+            "ஆண்டவரிடம் கொள்ளும் அச்சமே ஞானத்தின் தொடக்கம்; ஞானத்தையும் நற்பயிற்சியையும் மூடரே அவமதிப்பர்.",
+
+        englishReference:
+            "Proverbs 1:7",
+
+        englishVerse:
+            "The fear of the Lord is the start of knowledge: but the foolish have no use for wisdom and teaching."
+    },
+
+    {
+        day: 2,
+
+        reference: "நீதிமொழிகள் 2:6",
+
+        verse:
+            "ஏனெனில் ஞானத்தை அளிப்பவர் ஆண்டவரே, அறிவிற்கும் விவேகத்திற்கும் ஊற்றானவர் அவரே.",
+
+        englishReference:
+            "Proverbs 2:6",
+
+        englishVerse:
+            "For the Lord gives wisdom; out of his mouth come knowledge and reason."
+    },
+
+    {
+        day: 3,
+
+        reference: "நீதிமொழிகள் 3:6",
+
+        verse:
+            "நீ எதைச் செய்தாலும் ஆண்டவரை மனத்தில் வைத்துச் செய்; அப்பொழுது அவர் உன் பாதைகளைச் செம்மையாக்குவார்.",
+
+        englishReference:
+            "Proverbs 3:6",
+
+        englishVerse:
+            "In all your ways give ear to him, and he will make straight your footsteps."
+    },
+
+    {
+        day: 4,
+
+        reference: "நீதிமொழிகள் 4:23",
+
+        verse:
+            "விழிப்பாயிருந்து உன் இதயத்தைக் காவல் செய்; ஏனெனில், அதனின்று பிறப்பவை உன் வாழ்க்கையின் போக்கை உறுதிசெய்யும்.",
+
+        englishReference:
+            "Proverbs 4:23",
+
+        englishVerse:
+            " And keep watch over your heart with all care; so you will have life."
+    },
+
+    {
+        day: 5,
+
+        reference: "நீதிமொழிகள் 5:21",
+
+        verse:
+            "மனிதரின் வழிகளுள் ஒன்றும் ஆண்டவர் கண்களுக்குத் தப்புவதில்லை; அவர்களுடைய பாதைகளையெல்லாம் அவர் சீர்தூக்கிப் பார்க்கின்றார்.",
+
+        englishReference:
+            "Proverbs 5:21",
+
+        englishVerse:
+            "For a man’s ways are before the eyes of the Lord, and he puts all his goings in the scales."
+    },
+
+     {
+        day: 6,
+
+        reference: "நீதிமொழிகள் 6:20",
+
+        verse:
+            "பிள்ளாய்! உன் தந்தையின் கட்டளையைக் கடைப்பிடி; தாயின் அறிவுரையைப் புறக்கணியாதே.",
+
+        englishReference:
+            "Proverbs 6:20",
+
+        englishVerse:
+            "My son, keep the rule of your father, and have in memory the teaching of your mother."
     }
 
 ];
